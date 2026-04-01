54665a167c650635036da1982946f78426ed7ffb
Berit Herstad <berit.herstad@gmail.com>
2026-04-01 18:21:31 +0200

    Update skillbar.js

--- js/skillbar.js
+++ js/skillbar.js
@@ -1,20 +1,35 @@
 const skillbar = () => {
   const skillBars = document.querySelectorAll(".skill");
-  skillBars.forEach((skillBar) => {
-    const fill = skillBar.querySelector(".skill-bar__fill");
-    const percentage = skillBar.querySelector(".skill-percent");
-    const progress = parseInt(fill.getAttribute("data-progress"), 10);
-    fill.style.width = `${progress}%`;
 
-    let counter = 0;
-    const interval = setInterval(() => {
-      if (counter <= progress) {
-        percentage.textContent = `${counter}%`;
-        counter++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 1500 / progress);
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const skillBar = entry.target;
+          const fill = skillBar.querySelector(".skill-bar__fill");
+          const percentage = skillBar.querySelector(".skill-percent");
+          const progress = parseInt(fill.getAttribute("data-progress"), 10);
+          fill.style.width = `${progress}%`;
+
+          let counter = 0;
+          const interval = setInterval(() => {
+            if (counter <= progress) {
+              percentage.textContent = `${counter}%`;
+              counter++;
+            } else {
+              clearInterval(interval);
+            }
+          }, 1500 / progress);
+
+          observer.unobserve(skillBar);
+        }
+      });
+    },
+    { threshold: 0.3 }
+  );
+
+  skillBars.forEach((skillBar) => {
+    observer.observe(skillBar);
   });
 };
 export default skillbar;
